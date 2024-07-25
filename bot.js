2381a37310bc888ce2860d51780ff804c01b9243
GilangAristaR <asepg2114@gmail.com>
2024-07-25 09:38:55 +0700

    first commit

---
+++ bot.js
@@ -0,0 +1,56 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+// Gantikan 'YOUR_TELEGRAM_BOT_TOKEN' dengan token bot yang Anda dapatkan dari BotFather
+const token = '7368378129:AAFyFLKlwNf4O_ffXdQ5dFmzTBGqwQdnnTs';
+const bot = new TelegramBot(token, { polling: true });
+
+// Menyambut anggota baru
+bot.on('new_chat_members', (msg) => {
+    const chatId = msg.chat.id;
+    const newMember = msg.new_chat_member;
+
+    bot.sendMessage(chatId, `WELCOME TO PAVOCOIN, ${newMember.first_name}!`);
+});
+
+// Fungsi untuk memulai bot dengan perintah /start
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Hello! Im a Welcome Bot.');
+});
+
+// Fungsi untuk menampilkan informasi Web
+bot.onText(/\/web/, (msg) => {
+    const chatId = msg.chat.id;
+    const webMessage = 'Our Official Website : https://pavocoin.xyz/';
+    bot.sendMessage(chatId, webMessage);
+});
+
+// Fungsi untuk menampilkan informasi Twitter
+bot.onText(/\/twitter/, (msg) => {
+    const chatId = msg.chat.id;
+    const twitterMessage = 'X : https://x.com/PavoCoint';
+    bot.sendMessage(chatId, twitterMessage);
+});
+
+// Fungsi untuk menampilkan informasi Telegram
+bot.onText(/\/telegram/, (msg) => {
+    const chatId = msg.chat.id;
+    const telegramMessage = 'Telegram : https://t.me/+c2z_F35p-CIxZGU5';
+    bot.sendMessage(chatId, telegramMessage);
+});
+
+// Fungsi untuk menampilkan informasi Pump
+bot.onText(/\/pump/, (msg) => {
+    const chatId = msg.chat.id;
+    const telegramMessage = 'Pump : https://pump.fun/ECxVCgBYZz6jqwJdnEwWTgByeSR9YXrtyGcr5TLyZ85t';
+    bot.sendMessage(chatId, telegramMessage);
+});
+
+// Fungsi untuk menampilkan informasi Martket NFT
+bot.onText(/\/market/, (msg) => {
+    const chatId = msg.chat.id;
+    const telegramMessage = 'Market NFT : https://market.pavocoin.xyz/';
+    bot.sendMessage(chatId, telegramMessage);
+});
+
+console.log('Bot is running...');
